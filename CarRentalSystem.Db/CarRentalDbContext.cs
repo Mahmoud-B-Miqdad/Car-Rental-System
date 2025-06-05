48806f39870ff5140cd2befe0b8bf7c113c62dbf
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-05 14:13:04 +0300

    Configure EF Core in Web project and add CarRentalDbContext with entity relationships.

---
+++ CarRentalSystem.Db/CarRentalDbContext.cs
@@ -0,0 +1,49 @@
+﻿using CarRentalSystem.Db.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalSystem.Db
+{
+    public class CarRentalDbContext : DbContext
+    {
+        public CarRentalDbContext(DbContextOptions<CarRentalDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Car> Cars => Set<Car>();
+        public DbSet<Booking> Bookings => Set<Booking>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.Property(c => c.PricePerDay)
+                      .HasPrecision(18, 2);
+            });
+
+            modelBuilder.Entity<Booking>(entity =>
+            {
+                entity.Property(b => b.TotalPrice)
+                      .HasPrecision(18, 2);
+            });
+
+            modelBuilder.Entity<Booking>()
+                .HasOne(b => b.User)
+                .WithMany(u => u.Bookings)
+                .HasForeignKey(b => b.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Booking>()
+                .HasOne(b => b.Car)
+                .WithMany(c => c.Bookings)
+                .HasForeignKey(b => b.CarId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

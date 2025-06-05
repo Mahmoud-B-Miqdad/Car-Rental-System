c7e3ead35cd943bec22841ea8d2b7a218878bf2c
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-05 14:16:35 +0300

    Recreate 'Models' folder and 'ErrorModel' class with default configuration.

--- CarRentalSystem.Web/Controllers/HomeController.cs
+++ CarRentalSystem.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using CarRentalSystem.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace CarRentalSystem.Web.Controllers
 {
@@ -19,6 +21,12 @@ namespace CarRentalSystem.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }

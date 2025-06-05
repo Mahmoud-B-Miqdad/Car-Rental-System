ca006648af5798c572ed92a934a5728881561609
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-05 13:49:47 +0300

    Add entities: User, Car, Booking with navigation properties.

---
+++ CarRentalSystem.Db/Entities/Booking.cs
@@ -0,0 +1,15 @@
+﻿namespace CarRentalSystem.Db.Entities
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CarId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public User User { get; set; } = null!;
+        public Car Car { get; set; } = null!;
+    }
+}
---
+++ CarRentalSystem.Db/Entities/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace CarRentalSystem.Db.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public int Year { get; set; }
+        public decimal PricePerDay { get; set; }
+        public DateTime AvailableFromDate { get; set; }
+        public DateTime AvailableToDate { get; set; }
+
+        public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+    }
+}

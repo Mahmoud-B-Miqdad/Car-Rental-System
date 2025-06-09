73ff5a8d6c4add026573840b216f3e6914adb839
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-06-09 09:43:20 +0300

    Add CarController and View for displaying available cars - Created CarController with Index action to fetch available cars. - Added view to display car details with availability status. - Updated navigation bar with link to Available Cars page.

---
+++ CarRentalSystem.Web/Controllers/CarController.cs
@@ -0,0 +1,23 @@
+﻿using CarRentalSystem.Web.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalSystem.Web.Controllers
+{
+    [Authorize]
+    public class CarController : Controller
+    {
+        private readonly ICarService _carService;
+
+        public CarController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var cars = await _carService.GetAllCarsAsync();
+            return View(cars);
+        }
+    }
+}
